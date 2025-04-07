25f0951bbe45e721adac7cf628d209489731ab69
Hrishabh <hrishabhgautam480@gmail.com>
2025-04-07 15:21:13 +0530

    calculator class and test files

---
+++ sqld1/src/main/java/com/trivium/Calculator.java
@@ -0,0 +1,22 @@
+package com.trivium;
+/*this is a development code */
+public class Calculator {
+ //this is add method 
+ public int add(int a , int b) {
+	 return a+b;
+	 
+ }
+ //this is a subtract method
+ public int sub(int a , int b) {
+	 return a-b;
+ }
+ //this is a multiplication method
+ public int multiply(int a , int b) {
+	 return a*b;
+	 
+ }
+ //this is divide method
+ public int divide(int a ,int b) {
+	 return a/b;
+ }
+}
---
+++ sqld1/src/test/java/com/trivium/CalculatorTest.java
@@ -0,0 +1,34 @@
+package com.trivium;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/* This is a unit  test code for the Calculator class */
+ class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+     void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+      
+    }
+
+    @Test
+     void testSub() {
+        assertEquals(1, calc.sub(3, 2));
+
+    }
+    @Test
+     void testMultiply() {
+        assertEquals(6, calc.multiply(2, 3));
+        
+    }
+
+    @Test
+     void testDivide() {
+        assertEquals(2, calc.divide(6, 3));
+        
+    }
+}
