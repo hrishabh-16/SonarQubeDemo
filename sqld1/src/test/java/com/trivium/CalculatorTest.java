0ed9e398f716831dc7c77fd160c798468206296b
Hrishabh <hrishabhgautam480@gmail.com>
2025-04-07 15:35:36 +0530

    commented test cases

--- sqld1/src/test/java/com/trivium/CalculatorTest.java
+++ sqld1/src/test/java/com/trivium/CalculatorTest.java
@@ -9,22 +9,22 @@ import org.junit.jupiter.api.Test;
 
     Calculator calc = new Calculator();
 
-    @Test
-     void testAdd() {
-        assertEquals(5, calc.add(2, 3));
-      
-    }
-
-    @Test
-     void testSub() {
-        assertEquals(1, calc.sub(3, 2));
-
-    }
-    @Test
-     void testMultiply() {
-        assertEquals(6, calc.multiply(2, 3));
-        
-    }
+//    @Test
+//     void testAdd() {
+//        assertEquals(5, calc.add(2, 3));
+//      
+//    }
+//
+//    @Test
+//     void testSub() {
+//        assertEquals(1, calc.sub(3, 2));
+//
+//    }
+//    @Test
+//     void testMultiply() {
+//        assertEquals(6, calc.multiply(2, 3));
+//        
+//    }
 
     @Test
      void testDivide() {
